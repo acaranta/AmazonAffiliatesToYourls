e117f9a09aaf9df04421f074c620497eccf420e3
Arthur Caranta <arthur@caranta.com>
2013-12-06 14:55:56 +0100

    initial commit of working version

---
+++ AMZAff2Yourls.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl
+
+##############################################################################################
+# Copyright 2009 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file 
+# except in compliance with the License. A copy of the License is located at
+#
+#       http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under the License. 
+#
+#############################################################################################
+#
+#  Amazon Product Advertising API
+#  Signed Requests Sample Code
+#
+#  API Version: 2009-03-31
+#
+#############################################################################################
+
+use strict;
+
+use Data::Dumper;
+
+use RequestSignatureHelper;
+use LWP::UserAgent;
+use XML::Simple;
+use URI::Escape;
+
+use Config::Tiny ;
+my $Config = Config::Tiny->new();
+$Config = Config::Tiny->read( './AMZ2Yourls.ini' );
+my $AWSId = $Config->{AMZ}->{AWSId};
+my $AWSSecret = $Config->{AMZ}->{AWSSecret};
+my $AWSAssociateTag = $Config->{AMZ}->{AWSAssociateTag};
+my $AWSEndPoint = $Config->{AMZ}->{AWSEndPoint};
+my $YourlsId = $Config->{YOURLS}->{YOURLSSignature};
+my $YourlsEndPoint = $Config->{YOURLS}->{YOURLSEndPoint};
+
+# see if user provided ItemId on command-line
+my $itemId = shift @ARGV || '0545010225';
+
+# Set up the helper
+my $helper = new RequestSignatureHelper ( $AWSId, $AWSSecret, $AWSEndPoint);
+
+# A simple ItemLookup request
+my $request = {
+    Service => 'AWSECommerceService',
+    Operation => 'ItemLookup',
+    Version => '2009-03-31',
+    ItemId => $itemId,
+    ResponseGroup => 'Small',
+	AssociateTag => $AWSAssociateTag,
+};
+
+# Sign the request
+my $signedRequest = $helper->sign($request);
+
+# We can use the helper's canonicalize() function to construct the query string too.
+my $queryString = $helper->canonicalize($signedRequest);
+my $url = "http://$AWSEndPoint/onca/xml?" . $queryString;
+#print "Sending request to URL: $url \n";
+
+my $ua = new LWP::UserAgent();
+my $response = $ua->get($url);
+my $content = $response->content();
+#print "Recieved Response: $content \n";
+
+my $xmlParser = new XML::Simple();
+my $xml = $xmlParser->XMLin($content);
+
+#print "Parsed XML is: " . Dumper($xml) . "\n";
+my $signedurl ;
+my $title ;
+if ($response->is_success()) {
+    $title = $xml->{Items}->{Item}->{ItemAttributes}->{Title};
+    $signedurl = $xml->{Items}->{Item}->{DetailPageURL} ;
+} else {
+    my $error = findError($xml);
+    if (defined $error) {
+		#print "Error: " . $error->{Code} . ": " . $error->{Message} . "\n";
+    } else {
+		#print "Unknown Error!\n";
+    }
+	exit(1) ;
+}
+
+my $request = {
+	action=> 'shorturl',
+	url =>  uri_unescape($signedurl),
+	output => 'xml',
+};
+
+my $url = $YourlsEndPoint."/yourls-api.php?action=".$request->{'action'}."&url=".$request->{url}."&output=".$request->{output}."&signature=".$YourlsId;
+
+my $ua = new LWP::UserAgent();
+my $response = $ua->get($url);
+my $content = $response->content();
+
+my $xmlParser = new XML::Simple();
+my $xml = $xmlParser->XMLin($content);
+
+my $shorturl ;
+
+if ($response->is_success()) {
+	$shorturl = $xml->{shorturl} ;
+}
+
+
+    print "Item $itemId is titled \"$title\"\n";
+	print "Signed URL : ".uri_unescape($signedurl)."\n" ;
+	print "Short URL : $shorturl\n" ;
+
+
+
+
+
+
+
+sub findError {
+	my $xml = shift;
+
+	return undef unless ref($xml) eq 'HASH';
+
+	if (exists $xml->{Error}) { return $xml->{Error}; };
+
+	for (keys %$xml) {
+		my $error = findError($xml->{$_});
+		return $error if defined $error;
+	}
+
+	return undef;
+}
+
+
+
+
---
+++ RequestSignatureHelper.pm
@@ -0,0 +1,112 @@
+package RequestSignatureHelper;
+
+use strict;
+use warnings;
+use utf8;
+
+use Data::Dumper;
+
+use Digest;
+use Digest::SHA qw(hmac_sha256_base64);
+use URI::Escape qw(uri_escape_utf8);
+
+# set this to 1 if you want to see debugging output, 0 otherwise.
+my $DEBUG = 0;
+
+use base 'Exporter';
+
+our $kAWSAccessKeyId = 'AWSAccessKeyId';
+our $kAWSSecretKey   = 'AWSSecretKey';
+our $kEndPoint       = 'EndPoint';
+our $kRequestMethod  = 'RequestMethod';
+our $kRequestUri     = 'RequestUri';
+
+our $kTimestampParam		= 'Timestamp';
+our $kSignatureParam		= 'Signature';
+our $kSignatureVersionParam	= 'SignatureVersion';
+our $kSignatureVersionValue	= '2';
+our $kSignatureMethodParam	= 'SignatureMethod';
+our $kSignatureMethodValue	= 'HmacSHA256';
+
+use constant kUriEscapeRegex => '^A-Za-z0-9\-_.~';
+
+sub new {
+    my ($class, $AWSAccessId, $AWSSecret, $AWSEndPoint) = @_;
+    
+    my $self = {};
+    
+	$kAWSAccessKeyId = $AWSAccessId ;
+	$kAWSSecretKey = $AWSSecret ;
+    $kEndPoint	= lc($AWSEndPoint);
+    $kRequestMethod	= 'GET' ;
+    $kRequestUri	= '/onca/xml' ;
+
+    bless $self, $class;
+
+    return $self;
+}
+
+sub sign {
+    my ($self, $params) = @_;
+    
+    $params->{AWSAccessKeyId} = $kAWSAccessKeyId;
+    $params->{Timestamp} = $self->generateTimestamp() ;
+    my $canonical = $self->canonicalize($params);
+    my $stringToSign = 
+	$kRequestMethod    . "\n" . 
+	$kEndPoint	    . "\n" . 
+	$kRequestUri	    . "\n" . 
+	$canonical;
+    
+    # calculate the signature value and add it to the request.
+    my $signature = $self->digest($stringToSign);
+    $params->{Signature} = $signature;
+
+    debug ("signature: \"$signature\"\n");
+    debug ("final signed request: " . Dumper($params));
+    
+    return $params;
+}
+
+sub generateTimestamp {
+    return sprintf("%04d-%02d-%02dT%02d:%02d:%02d.000Z",
+       sub {    ($_[5]+1900,
+                 $_[4]+1,
+                 $_[3],
+                 $_[2],
+                 $_[1],
+                 $_[0])
+           }->(gmtime(time)));
+}
+
+sub escape {
+    my ($self, $x) = @_;
+    return uri_escape_utf8($x, +kUriEscapeRegex);
+}
+
+sub digest {
+    my ($self, $x) = @_;
+    my $digest = hmac_sha256_base64 ($x, $kAWSSecretKey);
+
+    return $digest . "=";
+}
+
+sub canonicalize {
+    my ($self, $params) = @_;
+    
+    my @parts = ();
+    while (my ($k, $v) = each %$params) {
+	my $x = $self->escape($k) . "=" . $self->escape($v);
+	push @parts, $x;
+    }
+
+    my $out = join ("&", sort @parts);
+    return $out;
+}
+
+sub debug {
+    if ($DEBUG) { print STDERR shift }
+}
+
+1;
+
